e66133074da2c679167bb18820ca0f7fa4dd40be
j-stillman <j.stillman2000@gmail.com>
2024-03-20 17:11:39 -0500

    changes made to startercode. Experimentation with displaying variables onscreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// Note: the code returned from this function App() is NOT HTML.
+// It looks very much like HTML, but it's actually a language called JSX.
+// A translator/compiler called Babel takes the JSX code and turns it into HTML whenever 
+// the file is saved.
+// Also of note: notice the use of "className" rather than "class". Since "class" is reserved
+// in Javascript, we cannot use class as though it were regular HTML. So we have to use className instead.
 function App() {
+
+  // You can write regular old Javascript outside the return statements of these functions
+  const title = "Welcome to the Website.";
+  const myNumber = 45;
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      <h1>
+        { title }
+      </h1>
+      <p> Displaying an array of [1, 2, 4, 8] looks like: { [1, 2, 4, 8] } </p>
+      <p> Displaying a random number between 0 and 10 looks like: { Math.random() * 10 } </p>
+      <p> Displaying a number looks like: { myNumber }</p>
+
+    {/* Below is the original starter code. Keeping it for reference for now */}
+    {/* 
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +39,7 @@ function App() {
           Learn React
         </a>
       </header>
+      */}
     </div>
   );
 }
